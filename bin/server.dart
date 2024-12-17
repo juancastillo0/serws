5dbabf5a7898c88c559fc5a4ccd36cfc7a27c6af
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2024-12-17 18:32:58 -0500

    shelf server implementation from initial api

--- bin/server.dart
+++ bin/server.dart
@@ -1,13 +1,10 @@
 import 'dart:io';
 
+import 'package:serws/serws.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
-
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
@@ -21,10 +18,25 @@ Response _echoHandler(Request request) {
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
+  final servers = ServerResponders(password: 'password');
+  final responseHandlers = ResponderHandlers(servers: servers);
+  final wsHandler = webSocketHandler(responseHandlers.createServerHandler);
+
+// Configure routes.
+  final router = Router()
+    ..get('/', _rootHandler)
+    ..get('/echo/<message>', _echoHandler)
+    ..all('/wscall', responseHandlers.wsCallHandler)
+    ..all('/call', responseHandlers.callHandler)
+    ..all('/subscribe', wsHandler);
 
   // Configure a pipeline that logs requests.
   final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+      Pipeline().addMiddleware(logRequests()).addHandler(router.call);
+
+  // shelf_io.serve(handler, 'localhost', 8080).then((server) {
+  //   print('Serving at ws://${server.address.host}:${server.port}');
+  // });
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
